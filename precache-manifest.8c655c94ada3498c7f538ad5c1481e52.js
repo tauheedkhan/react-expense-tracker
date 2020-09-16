c9d2a237839ce47524bbf2d6af65a62823568045
Tauheed Khan <tauheedkhan@outlook.com>
2020-09-16 21:31:28 +0530

    Updates

---
+++ precache-manifest.8c655c94ada3498c7f538ad5c1481e52.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8cc3188d30f784f09df9b6f584e66fa1",
+    "url": "/tauheedkhan/react-expense-tracker/index.html"
+  },
+  {
+    "revision": "bddab60fd2542441e821",
+    "url": "/tauheedkhan/react-expense-tracker/static/css/main.d0e32a8e.chunk.css"
+  },
+  {
+    "revision": "33556b952ec193449c6c",
+    "url": "/tauheedkhan/react-expense-tracker/static/js/2.80f3d389.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tauheedkhan/react-expense-tracker/static/js/2.80f3d389.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bddab60fd2542441e821",
+    "url": "/tauheedkhan/react-expense-tracker/static/js/main.5fc90735.chunk.js"
+  },
+  {
+    "revision": "46c8cc3055e54541cf3c",
+    "url": "/tauheedkhan/react-expense-tracker/static/js/runtime-main.cde1981b.js"
+  }
+]);
